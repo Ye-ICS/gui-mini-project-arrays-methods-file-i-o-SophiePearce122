5f09da479c9c9a736ee094e421f6ab48bfad6e8f
SophiePearce122 <sp9aj09@limestone.on.ca>
2025-11-25 12:22:16 -0500

    Created lightboxes
    Using void

--- App.java
+++ App.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.CheckBox;
@@ -17,7 +18,9 @@ import javafx.scene.Node;
  */
 public class App extends Application {
     CheckBox light = new CheckBox();
-
+    HBox rowlight;
+    VBox lightbox = new VBox();
+    
     public static void main(String[] args) {
         launch(args);
     }
@@ -30,12 +33,9 @@ public class App extends Application {
 
         Label promptLabel = new Label();
         promptLabel.setText("Welcome to lights out!");
+        createlightboxes();
 
-        Node lightbox = createlightboxes();
-
-        TextField thoughtsBox = new TextField();
-        thoughtsBox.setMaxWidth(150);
-        thoughtsBox.setPromptText("type here");
+      
         
         TextArea messageBox = new TextArea();
         messageBox.setEditable(false);
@@ -44,19 +44,27 @@ public class App extends Application {
         submissionBtn.setText("Submit");
 
         // Set up reactions (aka callbacks).
-        submissionBtn.setOnAction(event -> onSubmitThought(thoughtsBox, messageBox));
 
         // Add components to the content box.
         contentBox.getChildren().add(promptLabel);
-        contentBox.getChildren().add(thoughtsBox);
-        contentBox.getChildren().add(submissionBtn);
-        contentBox.getChildren().add(messageBox);
-
+        contentBox.getChildren().add(lightbox);
         // Set up the window and display it.
         Scene scene = new Scene(contentBox, 300, 200);
         stage.setScene(scene);
         stage.setTitle("Amazing App 2000");
         stage.show();
+
+    } 
+    void createlightboxes() {
+        for (int i =0; i < 5; i++){
+            rowlight = new HBox();
+            for (int j=0; j < 5; j++){
+                light = new CheckBox();
+                rowlight.getChildren().add(light);
+            }
+           lightbox.getChildren().add(rowlight);
+            
+        } 
 
     }
 
@@ -65,12 +73,5 @@ public class App extends Application {
      * @param inputBox  The TextField where the user types their thought.
      * @param outputBox The TextArea where the submitted thoughts are displayed.
      */
-    void onSubmitThought(TextField inputBox, TextArea outputBox) {
-        String text = inputBox.getText();
-        inputBox.clear();
-        System.out.println("Interesting thought: " + text);
-        outputBox.appendText("Interesting thought: " + text + "\n");
-    } Node createlightboxes(){
-
-    }
+    
 }
