65e34a3ebb69f44ccb4159f0ba8995c978e8c624
SophiePearce122 <sp9aj09@limestone.on.ca>
2025-11-26 12:47:52 -0500

    Removed textfield

--- App.java
+++ App.java
@@ -4,7 +4,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
